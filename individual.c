f012a223f967efc0f3b6444c58ff9f79734469cb
Atikahznl <2021886126@student.uitm.edu.my>
2023-04-20 02:39:14 +0000

    commit for individual

--- individual.c
+++ individual.c
@@ -20,34 +20,31 @@ int main() {
         	perror("Failed to pipe");
         	exit(1);
     	}
-
-    	for (int i = 0; i < 5; i++) {
+	printf("Enter a message to send to child : ");
+        fgets(message, sizeof(message), stdin);
+    	
+	for (int i = 0; i <5; i++) {
         	pid_t pid = fork();
         	if (pid == -1) {
-            	perror("Failed to fork");
-            	exit(1);
+            		perror("Failed to fork");
+            		exit(1);
         	} else if (pid == 0) {
-            	close(pipefd[1]);
-            	read(pipefd[0], message, sizeof(message));
-            	printf("Child %d Message received: %s\n", i, message);
-		close(pipefd[0]);
-            	exit(0);
+            		close(pipefd[1]);
+            		read(pipefd[0], message, sizeof(message));
+            		printf("Child %d Message received: %s\n", i+1, message);
+			close(pipefd[0]);
+            		exit(0);
         	} else {
-            	child[i] = pid;
-        }
-    }
-
-    	printf("Enter a message to send to children: ");
-    	fgets(message, sizeof(message), stdin);
-    	close(pipefd[0]);
-	for (int i = 0; i < 5; i++) {
-		printf("Parent writing to child %d\n",i);
-        	write(pipefd[1], message, sizeof(message));
+            		child[i] = pid;
+			write(pipefd[1], message, sizeof(message));
+			printf("Parent writing to child %d\n", i+1);
+		}
+    	}
+	for (int i = 0; i <5; i++) {
+		wait(NULL);
    	}
-	for(int i=0;i<5;i++){
-		waitpid(child[i],NULL,0);
-	}
-   	close(pipefd[1]);
+	close(pipefd[0]);
+	close(pipefd[1]);
     	return 0;
 }
 
